8ce9f052dd0964e2597de1d29a00ff5d298e0879
imboredandsleepy <77176203+imboredandsleepy@users.noreply.github.com>
2021-02-05 17:54:06 -0500

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,9 @@
+class log extends baseclass{
+  constructor(x,y,height,and){
+    super(x,y,20,height,and);
+    this.image = loadImage("sprites/wood2.png")
+    Matter.body.setAngle(this.body,angle);
+    
+    
+  }
+}
